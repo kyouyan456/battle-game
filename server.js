4fa6ba208e0493b45b7b89a4e073eab21ae7ef4c
kyouyan456 <kyouyan1115@gmail.com>
2026-09-14 23:52:38 +0900

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const wss = new WebSocketServer({ server });
 
 const rooms = new Map();
 
+app.use(
+ express.static(__dirname)
+);
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "battle_game.html"));
 });
